8cabdd56156566874ee82248fbfd966f26f8d3c0
audderall <155036500+audderall@users.noreply.github.com>
2026-06-16 18:59:36 +0530

    codebase cleanup, add text files

--- core/bump.py
+++ core/bump.py
@@ -1,20 +1,12 @@
-import asyncio
-from datetime import datetime
-
 async def bump(bot, channel_id, application_id):
     channel = bot.get_channel(int(channel_id))
     if channel is None:
-        print(f"channel {channel_id} is not found")
+        raise ValueError(f"channel {channel_id} not found")
+
+    command_list = await channel.application_commands()
+    for cmd in command_list:
+        if cmd.application_id == int(application_id):
+            await cmd()
+            return channel_id
         return False
-    try:
-        command_list = await channel.application_commands()
-        for cmd in command_list:
-            if cmd.application_id == int(application_id):
-                await cmd()
-                print(f"bumped {channel_id} at {datetime.now()}")
-                return True
-        print("error")
-        return False
-    except Exception as e:
-        print(f"exception {e}")
-        return False
+    raise ValueError(f"application {application_id} not found in channel {channel_id}")
--- main.py
+++ main.py
@@ -1,24 +1,39 @@
-from datetime import datetime
-import discord
 import logging
 import os
+from datetime import datetime
+
+import discord
 from dotenv import load_dotenv
+
 from core.bump import bump
 
 logging.disable(logging.INFO)
 
+
 load_dotenv()
 TOKEN = os.getenv("USER_TOKEN")
-DISBOARD_APP_ID="302050872383242240"
-CHANNEL_ID=int(os.getenv("CHANNEL_ID"))
+DISBOARD_APP_ID = "302050872383242240"
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 
 client = discord.Client()
 
 print("logging in...")
+
+
 @client.event
 async def on_ready():
-    print(f"logged in as {client.user} at {datetime.now()}")
-    await bump(client, CHANNEL_ID, DISBOARD_APP_ID)
-    await client.close()
+    print(f"logged in as {client.user} at {now()}")
+    try:
+        channel_id = await bump(client, CHANNEL_ID, DISBOARD_APP_ID)
+        print(f"bumped successfully in channel {channel_id} at {now()}")
+    except Exception as e:
+        print(e)
+    finally:
+        await client.close()
+
+
+def now():
+    return datetime.now().replace(microsecond=0)
+
 
 client.run(TOKEN)
